5592e6e51308ca74474a57d2a2ea80926c89bd43
Rod Whitby <rod@whitby.id.au>
2010-11-21 23:04:40 -0500

    Initial version

---
+++ src/luna_methods.c
@@ -0,0 +1,563 @@
+/*=============================================================================
+ Copyright (C) 2010 WebOS Internals <support@webos-internals.org>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ =============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <syslog.h>
+
+#include "luna_service.h"
+#include "luna_methods.h"
+
+#define ALLOWED_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-"
+
+#define API_VERSION "1"
+
+static char file_buffer[CHUNKSIZE+CHUNKSIZE+1];
+static char file_esc_buffer[MAXBUFLEN];
+
+//
+// Escape a string so that it can be used directly in a JSON response.
+// In general, this means escaping quotes, backslashes and control chars.
+// It uses the static esc_buffer, which must be twice as large as the
+// largest string this routine can handle.
+//
+static char *json_escape_str(char *str, char *esc_buffer) {
+  const char *json_hex_chars = "0123456789abcdef";
+
+  // Initialise the output buffer
+  strcpy(esc_buffer, "");
+
+  // Check the constraints on the input string
+  if (strlen(str) > MAXBUFLEN) return (char *)esc_buffer;
+
+  // Initialise the pointers used to step through the input and output.
+  char *resultsPt = (char *)esc_buffer;
+  int pos = 0, start_offset = 0;
+
+  // Traverse the input, copying to the output in the largest chunks
+  // possible, escaping characters as we go.
+  unsigned char c;
+  do {
+    c = str[pos];
+    switch (c) {
+    case '\0':
+      // Terminate the copying
+      break;
+    case '\b':
+    case '\n':
+    case '\r':
+    case '\t':
+    case '"':
+    case '\\': {
+      // Copy the chunk before the character which must be escaped
+      if (pos - start_offset > 0) {
+	memcpy(resultsPt, str + start_offset, pos - start_offset);
+	resultsPt += pos - start_offset;
+      };
+      
+      // Escape the character
+      if      (c == '\b') {memcpy(resultsPt, "\\b",  2); resultsPt += 2;} 
+      else if (c == '\n') {memcpy(resultsPt, "\\n",  2); resultsPt += 2;} 
+      else if (c == '\r') {memcpy(resultsPt, "\\r",  2); resultsPt += 2;} 
+      else if (c == '\t') {memcpy(resultsPt, "\\t",  2); resultsPt += 2;} 
+      else if (c == '"')  {memcpy(resultsPt, "\\\"", 2); resultsPt += 2;} 
+      else if (c == '\\') {memcpy(resultsPt, "\\\\", 2); resultsPt += 2;} 
+
+      // Reset the start of the next chunk
+      start_offset = ++pos;
+      break;
+    }
+
+    default:
+      
+      // Check for "special" characters
+      if ((c < ' ') || (c > 127)) {
+
+	// Copy the chunk before the character which must be escaped
+	if (pos - start_offset > 0) {
+	  memcpy(resultsPt, str + start_offset, pos - start_offset);
+	  resultsPt += pos - start_offset;
+	}
+
+	// Insert a normalised representation
+	sprintf(resultsPt, "\\u00%c%c",
+		json_hex_chars[c >> 4],
+		json_hex_chars[c & 0xf]);
+
+	// Reset the start of the next chunk
+	start_offset = ++pos;
+      }
+      else {
+	// Just move along the source string, without copying
+	pos++;
+      }
+    }
+  } while (c);
+
+  // Copy the final chunk, if required
+  if (pos - start_offset > 0) {
+    memcpy(resultsPt, str + start_offset, pos - start_offset);
+    resultsPt += pos - start_offset;
+  } 
+
+  // Terminate the output buffer ...
+  memcpy(resultsPt, "\0", 1);
+
+  // and return a pointer to it.
+  return (char *)esc_buffer;
+}
+
+//
+// A dummy method, useful for unimplemented functions or as a status function.
+// Called directly from webOS, and returns directly to webOS.
+//
+bool dummy_method(LSHandle* lshandle, LSMessage *message, void *ctx) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  if (!LSMessageReply(lshandle, message, "{\"returnValue\": true}", &lserror)) goto error;
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+//
+// Return the current API version of the service.
+// Called directly from webOS, and returns directly to webOS.
+//
+bool version_method(LSHandle* lshandle, LSMessage *message, void *ctx) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  if (!LSMessageReply(lshandle, message, "{\"returnValue\": true, \"version\": \"" VERSION "\", \"apiVersion\": \"" API_VERSION "\"}", &lserror)) goto error;
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+//
+// Run a shell command, and return the output in-line in a buffer for returning to webOS.
+// The global run_command_buffer must be initialised before calling this function.
+// The return value says whether the command executed successfully or not.
+//
+static bool run_command(char *command, bool escape, char *buffer) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  char esc_buffer[MAXBUFLEN];
+
+  // Local buffers to store the current and previous lines.
+  char line[MAXLINLEN];
+
+  // fprintf(stderr, "Running command %s\n", command);
+
+  // buffer is assumed to be initialised, ready for strcat to append.
+
+  // Is this the first line of output?
+  bool first = true;
+
+  bool array = false;
+
+  // Start execution of the command, and read the output.
+  FILE *fp = popen(command, "r");
+
+  // Return immediately if we cannot even start the command.
+  if (!fp) {
+    return false;
+  }
+
+  // Loop through the output lines
+  while (fgets(line, sizeof line, fp)) {
+
+    // Chomp the newline
+    char *nl = strchr(line,'\n'); if (nl) *nl = 0;
+
+    // Add formatting breaks between lines
+    if (first) {
+      if (buffer[strlen(buffer)-1] == '[') {
+	array = true;
+      }
+      first = false;
+    }
+    else {
+      if (array) {
+	strcat(buffer, ", ");
+      }
+      else {
+	strcat(buffer, "<br>");
+      }
+    }
+    
+    // Append the unfiltered output to the buffer.
+    if (escape) {
+      if (array) {
+	strcat(buffer, "\"");
+      }
+      strcat(buffer, json_escape_str(line, esc_buffer));
+      if (array) {
+	strcat(buffer, "\"");
+      }
+    }
+    else {
+      strcat(buffer, line);
+    }
+  }
+  
+  // Check the close status of the process
+  if (pclose(fp)) {
+    return false;
+  }
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  // %%% We need a way to distinguish command failures from LSMessage failures %%%
+  // %%% This may need to be true if we just want to ignore LSMessage failures %%%
+  return false;
+}
+
+//
+// Send a standard format command failure message back to webOS.
+// The command will be escaped.  The output argument should be a JSON array and is not escaped.
+// The additional text  will not be escaped.
+// The return value is from the LSMessageReply call, not related to the command execution.
+//
+static bool report_command_failure(LSHandle* lshandle, LSMessage *message, char *command, char *stdErrText, char *additional) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  char buffer[MAXBUFLEN];
+  char esc_buffer[MAXBUFLEN];
+
+  // Include the command that was executed, in escaped form.
+  snprintf(buffer, MAXBUFLEN,
+	   "{\"errorText\": \"Unable to run command: %s\"",
+	   json_escape_str(command, esc_buffer));
+
+  // Include any stderr fields from the command.
+  if (stdErrText) {
+    strcat(buffer, ", \"stdErr\": ");
+    strcat(buffer, stdErrText);
+  }
+
+  // Report that an error occurred.
+  strcat(buffer, ", \"returnValue\": false, \"errorCode\": -1");
+
+  // Add any additional JSON fields.
+  if (additional) {
+    strcat(buffer, ", ");
+    strcat(buffer, additional);
+  }
+
+  // Terminate the JSON reply message ...
+  strcat(buffer, "}");
+
+  // fprintf(stderr, "Message is %s\n", buffer);
+
+  // and send it.
+  if (!LSMessageReply(lshandle, message, buffer, &lserror)) goto error;
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+//
+// Run a simple shell command, and return the output to webOS.
+//
+static bool simple_command(LSHandle* lshandle, LSMessage *message, char *command) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  char run_command_buffer[MAXBUFLEN];
+
+  // Initialise the output buffer
+  strcpy(run_command_buffer, "{\"stdOut\": [");
+
+  // Run the command
+  if (run_command(command, true, run_command_buffer)) {
+
+    // Finalise the message ...
+    strcat(run_command_buffer, "], \"returnValue\": true}");
+
+    // fprintf(stderr, "Message is %s\n", run_command_buffer);
+
+    // and send it to webOS.
+    if (!LSMessageReply(lshandle, message, run_command_buffer, &lserror)) goto error;
+  }
+  else {
+
+    // Finalise the command output ...
+    strcat(run_command_buffer, "]");
+
+    // and use it in a failure report message.
+    if (!report_command_failure(lshandle, message, command, run_command_buffer+11, NULL)) goto end;
+  }
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+static bool read_file(LSHandle* lshandle, LSMessage *message, char *filename, bool subscribed) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  FILE * file = fopen(filename, "r");
+  if (!file) {
+    sprintf(file_buffer,
+	    "{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Cannot open %s\"}",
+	    filename);
+    
+    if (!LSMessageReply(lshandle, message, file_buffer, &lserror)) goto error;
+    return true;
+  }
+  
+  char chunk[CHUNKSIZE];
+  int chunksize = CHUNKSIZE;
+
+  syslog(LOG_DEBUG, "Reading file %s\n", filename);
+
+  fseek(file, 0, SEEK_END);
+  int filesize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  if (subscribed) {
+    if (sprintf(file_buffer,
+		"{\"returnValue\": true, \"filesize\": %d, \"chunksize\": %d, \"stage\": \"start\"}",
+		filesize, chunksize)) {
+
+      if (!LSMessageReply(lshandle, message, file_buffer, &lserror)) goto error;
+
+    }
+  }
+  else if (filesize < chunksize) {
+    chunksize = filesize;
+  }
+
+  int size;
+  int datasize = 0;
+  while ((size = fread(chunk, 1, chunksize, file)) > 0) {
+    datasize += size;
+    chunk[size] = '\0';
+    sprintf(file_buffer, "{\"returnValue\": true, \"size\": %d, \"contents\": \"", size);
+    strcat(file_buffer, json_escape_str(chunk, file_esc_buffer));
+    strcat(file_buffer, "\"");
+    if (subscribed) {
+      strcat(file_buffer, ", \"stage\": \"middle\"");
+    }
+    strcat(file_buffer, "}");
+
+    if (!LSMessageReply(lshandle, message, file_buffer, &lserror)) goto error;
+
+  }
+
+  if (!fclose(file)) {
+    if (subscribed) {
+      sprintf(file_buffer, "{\"returnValue\": true, \"datasize\": %d, \"stage\": \"end\"}", datasize);
+
+      if (!LSMessageReply(lshandle, message, file_buffer, &lserror)) goto error;
+
+    }
+  }
+  else {
+    sprintf(file_buffer, "{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Cannot close file\"}");
+
+    if (!LSMessageReply(lshandle, message, file_buffer, &lserror)) goto error;
+
+  }
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+bool getDbKind_method(LSHandle* lshandle, LSMessage *message, void *ctx) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  char filename[MAXLINLEN];
+
+  // Extract the id argument from the message
+  json_t *object = json_parse_document(LSMessageGetPayload(message));
+  json_t *id = json_find_first_label(object, "id");               
+  if (!id || (id->child->type != JSON_STRING) || (strspn(id->child->text, ALLOWED_CHARS) != strlen(id->child->text))) {
+    if (!LSMessageReply(lshandle, message,
+			"{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Invalid or missing id\"}",
+			&lserror)) goto error;
+    return true;
+  }
+
+  sprintf(filename, "/etc/palm/db/kinds/%s", id->child->text);
+
+  return read_file(lshandle, message, filename, false);
+
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+bool getDbPermission_method(LSHandle* lshandle, LSMessage *message, void *ctx) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  char filename[MAXLINLEN];
+
+  // Extract the id argument from the message
+  json_t *object = json_parse_document(LSMessageGetPayload(message));
+  json_t *id = json_find_first_label(object, "id");               
+  if (!id || (id->child->type != JSON_STRING) || (strspn(id->child->text, ALLOWED_CHARS) != strlen(id->child->text))) {
+    if (!LSMessageReply(lshandle, message,
+			"{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Invalid or missing id\"}",
+			&lserror)) goto error;
+    return true;
+  }
+
+  sprintf(filename, "/etc/palm/db/permissions/%s", id->child->text);
+
+  return read_file(lshandle, message, filename, false);
+
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+//
+// Handler for the impersonate service.
+//
+bool impersonate_handler(LSHandle* lshandle, LSMessage *reply, void *ctx) {
+  bool retVal;
+  LSError lserror;
+  LSErrorInit(&lserror);
+  LSMessage* message = (LSMessage*)ctx;
+  retVal = LSMessageRespond(message, LSMessageGetPayload(reply), &lserror);
+  LSMessageUnref(message);
+  if (!retVal) {
+    LSErrorPrint(&lserror, stderr);
+    LSErrorFree(&lserror);
+  }
+  return retVal;
+}
+
+//
+// Impersonate a call to the requested service and return the output to webOS.
+//
+bool impersonate_method(LSHandle* lshandle, LSMessage *message, void *ctx) {
+  bool retVal;
+  LSError lserror;
+  LSErrorInit(&lserror);
+  LSMessageRef(message);
+
+  // Extract the method argument from the message
+  json_t *object = json_parse_document(LSMessageGetPayload(message));
+  json_t *id = json_find_first_label(object, "id");               
+  if (!id || (id->child->type != JSON_STRING) || (strspn(id->child->text, ALLOWED_CHARS) != strlen(id->child->text))) {
+    if (!LSMessageReply(lshandle, message,
+			"{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Invalid or missing id\"}",
+			&lserror)) goto error;
+    return true;
+  }
+
+  // Extract the service argument from the message
+  object = json_parse_document(LSMessageGetPayload(message));
+  json_t *service = json_find_first_label(object, "service");               
+  if (!service || (service->child->type != JSON_STRING) || (strspn(service->child->text, ALLOWED_CHARS) != strlen(service->child->text))) {
+    if (!LSMessageReply(lshandle, message,
+			"{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Invalid or missing service\"}",
+			&lserror)) goto error;
+    return true;
+  }
+
+  // Extract the method argument from the message
+  object = json_parse_document(LSMessageGetPayload(message));
+  json_t *method = json_find_first_label(object, "method");               
+  if (!method || (method->child->type != JSON_STRING) || (strspn(method->child->text, ALLOWED_CHARS) != strlen(method->child->text))) {
+    if (!LSMessageReply(lshandle, message,
+			"{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Invalid or missing method\"}",
+			&lserror)) goto error;
+    return true;
+  }
+
+  // Extract the params argument from the message
+  object = json_parse_document(LSMessageGetPayload(message));
+  json_t *params = json_find_first_label(object, "params");               
+  if (!params || (params->child->type != JSON_OBJECT)) {
+    if (!LSMessageReply(lshandle, message,
+			"{\"returnValue\": false, \"errorCode\": -1, \"errorText\": \"Invalid or missing params\"}",
+			&lserror)) goto error;
+    return true;
+  }
+
+  char uri[MAXLINLEN];
+  sprintf(uri, "palm://%s/%s", service->child->text, method->child->text);
+
+  char *paramstring = NULL;
+  json_tree_to_string (params->child, &paramstring);
+  if (!LSCallFromApplication(priv_serviceHandle, uri, paramstring, id->child->text,
+			     impersonate_handler, message, NULL, &lserror)) goto error;
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+LSMethod luna_methods[] = {
+  { "status",		dummy_method },
+  { "version",		version_method },
+
+  { "getDbKind",	getDbKind_method },
+  { "getDbPermission",	getDbPermission_method },
+
+  { "impersonate",	impersonate_method },
+
+  { 0, 0 }
+};
+
+bool register_methods(LSPalmService *serviceHandle, LSError lserror) {
+  return LSPalmServiceRegisterCategory(serviceHandle, "/", NULL, luna_methods,
+				       NULL, NULL, &lserror);
+}

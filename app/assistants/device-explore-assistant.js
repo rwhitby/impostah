744d1dad51f3b2b6dc66f81d1464b08ad646d2c8
Rod Whitby <rod@whitby.id.au>
2010-12-28 12:39:38 +1030

    Added support for reporting the telephony platform

--- app/assistants/device-explore-assistant.js
+++ app/assistants/device-explore-assistant.js
@@ -20,6 +20,11 @@ function DeviceExploreAssistant()
 		label: $L("Device Profile"),
 		disabled: true
 	};
+
+	this.telephonyPlatformButtonModel = {
+		label: $L("Telephony Platform"),
+		disabled: true
+	};
 };
 
 DeviceExploreAssistant.prototype.setup = function()
@@ -32,21 +37,31 @@ DeviceExploreAssistant.prototype.setup = function()
 	this.iconElement.style.display = 'none';
 	this.spinnerElement = 		this.controller.get('spinner');
 	this.deviceProfileButton = this.controller.get('deviceProfileButton');
+	this.telephonyPlatformButton = this.controller.get('telephonyPlatformButton');
 	
 	// setup handlers
 	this.deviceProfileTapHandler = this.deviceProfileTap.bindAsEventListener(this);
 	this.getDeviceProfileHandler =	this.getDeviceProfile.bindAsEventListener(this);
+	this.telephonyPlatformTapHandler = this.telephonyPlatformTap.bindAsEventListener(this);
+	this.getTelephonyPlatformHandler =	this.getTelephonyPlatform.bindAsEventListener(this);
 	
 	// setup wigets
 	this.spinnerModel = {spinning: true};
 	this.controller.setupWidget('spinner', {spinnerSize: 'small'}, this.spinnerModel);
 	this.controller.setupWidget('deviceProfileButton', { }, this.deviceProfileButtonModel);
 	this.controller.listen(this.deviceProfileButton,  Mojo.Event.tap, this.deviceProfileTapHandler);
+	this.controller.setupWidget('telephonyPlatformButton', { }, this.telephonyPlatformButtonModel);
+	this.controller.listen(this.telephonyPlatformButton,  Mojo.Event.tap, this.telephonyPlatformTapHandler);
 	
-	this.deviceProfile = {};
-	this.request = ImpostahService.impersonate(this.getDeviceProfileHandler, "com.palm.configurator",
+	this.deviceProfile = false;
+	this.telephonyPlatform = false;
+
+	this.request1 = ImpostahService.impersonate(this.getDeviceProfileHandler, "com.palm.configurator",
 											   "com.palm.deviceprofile",
 											   "getDeviceProfile", {});
+	this.request2 = ImpostahService.impersonate(this.getTelephonyPlatformHandler, "com.palm.configurator",
+											   "com.palm.telephony",
+											   "platformQuery", {});
 };
 
 DeviceExploreAssistant.prototype.getDeviceProfile = function(payload)
@@ -58,9 +73,11 @@ DeviceExploreAssistant.prototype.getDeviceProfile = function(payload)
 
 	this.deviceProfile = payload.deviceInfo;
 
-	this.iconElement.style.display = 'inline';
-	this.spinnerModel.spinning = false;
-	this.controller.modelChanged(this.spinnerModel);
+	if (this.deviceProfile && this.telephonyPlatform) {
+		this.iconElement.style.display = 'inline';
+		this.spinnerModel.spinning = false;
+		this.controller.modelChanged(this.spinnerModel);
+	}
 
 	this.deviceProfileButtonModel.disabled = false;
 	this.controller.modelChanged(this.deviceProfileButtonModel);
@@ -70,6 +87,32 @@ DeviceExploreAssistant.prototype.deviceProfileTap = function(event)
 {
 	if (this.deviceProfile) {
 		this.controller.stageController.pushScene("item", "Device Profile", this.deviceProfile);
+	}
+};
+
+DeviceExploreAssistant.prototype.getTelephonyPlatform = function(payload)
+{
+	if (payload.returnValue === false) {
+		this.errorMessage('<b>Service Error (getTelephonyPlatform):</b><br>'+payload.errorText);
+		return;
+	}
+
+	this.telephonyPlatform = payload.extended;
+
+	if (this.deviceProfile && this.telephonyPlatform) {
+		this.iconElement.style.display = 'inline';
+		this.spinnerModel.spinning = false;
+		this.controller.modelChanged(this.spinnerModel);
+	}
+
+	this.telephonyPlatformButtonModel.disabled = false;
+	this.controller.modelChanged(this.telephonyPlatformButtonModel);
+};
+
+DeviceExploreAssistant.prototype.telephonyPlatformTap = function(event)
+{
+	if (this.telephonyPlatform) {
+		this.controller.stageController.pushScene("item", "Telephony Platform", this.telephonyPlatform);
 	}
 };
 
@@ -104,6 +147,8 @@ DeviceExploreAssistant.prototype.cleanup = function(event)
 {
 	this.controller.stopListening(this.deviceProfileButton,  Mojo.Event.tap,
 								  this.deviceProfileTapHandler);
+	this.controller.stopListening(this.telephonyPlatformButton,  Mojo.Event.tap,
+								  this.telephonyPlatformTapHandler);
 };
 
 // Local Variables:

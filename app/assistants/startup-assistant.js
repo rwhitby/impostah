f18210b594081210a49b6c23807708a824de0f64
Rod Whitby <rod@whitby.id.au>
2011-04-23 13:54:10 +1000

    Added the language and country selectors for profile creation. Added the activation scene (split from the palm profile scene). Added the ability to check email availability. Added the ability to override the MCC and MNC, without a SIM inserted.

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -11,7 +11,8 @@ function StartupAssistant(changelog)
     this.newMessages =
 	[
 	 // Don't forget the comma on all but the last entry
-	 { version: '0.7.1', log: [ 'Added the activation scene (split from the palm profile scene)',
+	 { version: '0.8.0', log: [ 'Added the language and country selectors for profile creation',
+								'Added the activation scene (split from the palm profile scene)',
 								'Added the ability to check email availability',
 								'Added the ability to override the MCC and MNC, without a SIM inserted' ] },
 	 { version: '0.7.0', log: [ 'Added the ability to install free (not paid) geo-restricted apps' ] },
